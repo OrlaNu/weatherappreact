e98551aad87a0fbb2fd2faedb5282d850a3c3d55
OrlaNu <orla_nugent@hotmail.co.uk>
2022-03-14 17:42:59 +0000

    Added rel="noreferrer" for React error

--- src/Socials.js
+++ src/Socials.js
@@ -4,7 +4,7 @@ import { FaGithub } from "react-icons/fa";
 export default function Socials(){
 return (
     <div>
-        <a href="https://github.com/OrlaNu/weatherappreact" target="_blank"><FaGithub /></a>
+        <a href="https://github.com/OrlaNu/weatherappreact" target="_blank" rel="noreferrer"><FaGithub /></a>
     </div>  
 );
 }
