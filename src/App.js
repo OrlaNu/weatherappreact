4d33e281be440f4cf43e93a6bef0a8c705cbb102
OrlaNu <orla_nugent@hotmail.co.uk>
2022-05-23 09:44:18 +0100

    Adding dark mode toggle and styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 
 import './App.css';
-import React from "react";
+import React, { useState } from "react";
 import Socials from './Socials'
 import WeatherSearch from './WeatherSearch';
+import { createContext } from "react"; 
+import ReactSwitch from 'react-switch';
+
+export const ThemeContext = createContext(null);
 
 export default function App(){
+  const [theme, setTheme] = useState("dark"); 
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"))
+  };
   return (
-  <div className="App"> 
-    <div className="container">
-      <WeatherSearch defaultCity = "London"/>
-      <Socials />
+  <ThemeContext.Provider value={{ theme, toggleTheme}}>
+    <div className="App" id={theme}> 
+      <div className="container">
+        <WeatherSearch defaultCity = "London"/>
+        <Socials />
+      <div className="switch">
+        <label> {theme === "light" ? "🌞 " : "🌙 "} </label>
+        <ReactSwitch onChange={toggleTheme} checked={theme ==="dark"}/>
+        
+      </div>
+      </div>
     </div>
-  </div>
+  </ThemeContext.Provider>
   );
 }
 
